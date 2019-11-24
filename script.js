a4afcb5c9a21a1f61591329ff0697a279877ee5c
Arntit-Zonia <arditzonia@gmail.com>
2019-11-24 20:51:01 +0000

    Added a blinking cursor animation fixed a logic error where the last character from the list wasn't included

--- script.js
+++ script.js
@@ -2,18 +2,21 @@ let index = 0;
 let count = 0;
 
 (function typewriting() {
-    const words = ["Self-Taught", "Web Developer"];
+    const words = ["a Self-Taught", "Web Developer"];
     const output = document.querySelector(".text-output");
     const currentWord = words[count];
 
-    if (index < currentWord.length) {
+    if (index <= currentWord.length) {
         output.textContent += currentWord.charAt(index);
         index++;
-    } else if (index === currentWord.length) {
+    }
+
+    if (index === currentWord.length + 1) {
         count++;
         index = 0;
         output.textContent = "";
     }
+
     if (count === words.length) count = 0;
 
     setTimeout(typewriting, 300);
