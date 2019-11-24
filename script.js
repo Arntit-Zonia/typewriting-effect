b9365866cb887b6a890c730346c4acd68f0cfb08
Arntit-Zonia <arditzonia@gmail.com>
2019-11-24 20:25:35 +0000

    implemented typewriting effect

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 let index = 0;
-let typeSpeed = 300;
-const words = ["Self-Taught", "Web Developer"];
-const output = document.querySelector(".text-output");
+let count = 0;
+
+(function typewriting() {
+    const words = ["Self-Taught", "Web Developer"];
+    const output = document.querySelector(".text-output");
+    const currentWord = words[count];
+
+    if (index < currentWord.length) {
+        output.textContent += currentWord.charAt(index);
+        index++;
+    } else if (index === currentWord.length) {
+        count++;
+        index = 0;
+        output.textContent = "";
+    }
+    if (count === words.length) count = 0;
+
+    setTimeout(typewriting, 300);
+})();
